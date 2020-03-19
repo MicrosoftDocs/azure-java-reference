5b6e9920670885ea32980c8218bbd86dc0e65d49
Scott Beddall <scbedd@microsoft.com>
2020-03-18 19:28:06 -0700

    fixed the script. correct directory now. time to test!

--- update_toc.ps1
+++ update_toc.ps1
@@ -5,7 +5,7 @@ Param(
 
 Write-Host $TargetFolder
 
-$results = Get-Content "$TargetFolder/unified-latest/docs-ref-mapping/reference-latest.yml","legacy-docs-toc-merge/toc.yml" 
+$results = Get-Content "$TargetFolder/unified-latest/docs-ref-mapping/reference-latest.yml","$TargetFolder/legacy-docs-toc-merge/toc.yml" 
 # needs to be another 
 $results | Set-Content "$TargetFolder/unified-latest/docs-ref-mapping/reference-latest.yml" 
 Copy-Item -Path "$TargetFolder/legacy-docs-toc-merge/*" -Destination "$TargetFolder/unified-latest/docs-ref-autogen"
