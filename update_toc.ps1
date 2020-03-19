ede74257f3dc1ff1c6050e293c10f33239d85e26
Scott Beddall <scbedd@microsoft.com>
2020-03-18 18:47:47 -0700

    have a powershell that works!

---
+++ update_toc.ps1
@@ -0,0 +1,21 @@
+# this should only be run if the generated toc.yml has been updated
+
+Param(
+  [string]$TargetFolder
+)
+
+$results = Get-Content "$TargetFolder/unified-latest/docs-ref-mapping/reference-latest.yml","legacy-docs-toc-merge/toc.yml" 
+# needs to be another 
+$results | Set-Content "$TargetFolder/unified-latest/docs-ref-mapping/reference-latest.yml" 
+Copy-Item -Path "$TargetFolder/legacy-docs-toc-merge/*" -Destination "$TargetFolder/unified-latest/docs-ref-autogen"
+
+
+# converted for main repo
+Param(
+  [string]$TargetFolder
+)
+
+$results = Get-Content "$TargetFolder/docs-ref-mapping/reference-latest.yml","legacy-docs-toc-merge/toc.yml" 
+# needs to be another 
+$results | Set-Content "$TargetFolder/docs-ref-mapping/reference-latest.yml" 
+Copy-Item -Path "$TargetFolder/legacy-docs-toc-merge/*" -Destination "$TargetFolder/docs-ref-autogen"
--- update_toc.py
+++
@@ -1,7 +0,0 @@
-# Assumptions
-#   This script is being executed in the MicrosoftDocs/azure-java-reference repository
-#   This script is running on at least python 2.7
-
-
-
-Get-Content "unified-latest/reference-latest.yml","legacy-docs-toc-merge/toc.yml" | Set-Content "univied-latest/reference-latest.yml" 
