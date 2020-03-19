95717430181de54b57027f7ad9a4b3b2728cf2c4
Scott Beddall <scbedd@microsoft.com>
2020-03-18 18:48:37 -0700

    committing the updated toc

--- update_toc.ps1
+++ update_toc.ps1
@@ -1,5 +1,4 @@
-# this should only be run if the generated toc.yml has been updated
-
+# this should only be run if the generated toc.yml has been updated, otherwise it'll just keep adding duplicates to the build
 Param(
   [string]$TargetFolder
 )
@@ -10,12 +9,12 @@ $results | Set-Content "$TargetFolder/unified-latest/docs-ref-mapping/reference-
 Copy-Item -Path "$TargetFolder/legacy-docs-toc-merge/*" -Destination "$TargetFolder/unified-latest/docs-ref-autogen"
 
 
-# converted for main repo
-Param(
-  [string]$TargetFolder
-)
+# # converted for main repo
+# Param(
+#   [string]$TargetFolder
+# )
 
-$results = Get-Content "$TargetFolder/docs-ref-mapping/reference-latest.yml","legacy-docs-toc-merge/toc.yml" 
-# needs to be another 
-$results | Set-Content "$TargetFolder/docs-ref-mapping/reference-latest.yml" 
-Copy-Item -Path "$TargetFolder/legacy-docs-toc-merge/*" -Destination "$TargetFolder/docs-ref-autogen"
+# $results = Get-Content "$TargetFolder/docs-ref-mapping/reference-latest.yml","legacy-docs-toc-merge/toc.yml" 
+# # needs to be another 
+# $results | Set-Content "$TargetFolder/docs-ref-mapping/reference-latest.yml" 
+# Copy-Item -Path "$TargetFolder/legacy-docs-toc-merge/*" -Destination "$TargetFolder/docs-ref-autogen"
